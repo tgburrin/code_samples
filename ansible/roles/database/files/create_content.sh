12dc0b8c8724658afeaa4e49a2af7970c0516c74
Tim Burrington <tgburrin@protonmail.com>
2017-10-17 10:19:29 -0400

    Fixing content creation to account for client ids now.

--- ansible/roles/database/files/create_content.sh
+++ ansible/roles/database/files/create_content.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
+for c in testClientA testClientB testClientC testClientD testClientE
 do
-    psql -U tgburrin << EOF
-insert into content select uuid_generate_v1(), 'test${i}','test${i}';
+    CLIENT_ID=`psql -U tgburrin -t -A -c "select * from create_client('${c}')"`
+    for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
+    do
+        psql -t -A -U tgburrin << EOF
+select * from content_add('${CLIENT_ID}','${c}_${i}','url://${c}/${i}')
 EOF
+    done
 done
