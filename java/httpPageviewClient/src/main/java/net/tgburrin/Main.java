9c53b3c9d6a95d9416bfbcabd05d235f0ea567c9
Tim Burrington <tgburrin>
2022-10-25 10:35:57 -0400

    Fixing up the http client to use a more modern version of java.

--- java/httpPageviewClient/src/main/java/net/tgburrin/Main.java
+++ java/httpPageviewClient/src/main/java/net/tgburrin/Main.java
@@ -1,145 +1,144 @@
 package net.tgburrin;
 
 import java.io.IOException;
-
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
-
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.apache.commons.io.IOUtils;
-
-import org.apache.http.HttpStatus;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import com.google.gson.stream.JsonReader;
-import com.google.gson.JsonParser;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
 
 public class Main extends Thread {
-    private static AtomicInteger msgCounter = new AtomicInteger(0);
-    private static int statsTimeSec = 5;
+	private static AtomicInteger msgCounter = new AtomicInteger(0);
+	private static int statsTimeSec = 5;
 
-    private static String webHost = "webserver01:8080";
-    private static String apiVersion = "v1";
-    private static String urlBase = "/"+apiVersion+"/content";
+	private static String webHost = "webserver01:8080";
+	private static String apiVersion = "v1";
+	private static String urlBase = "/" + apiVersion + "/content";
 
-    private static int numberOfClients = 48;
-    private static int numberOfViews = 10000;
+	private static int numberOfClients = 48;
+	private static int numberOfViews = 10000;
 
-    private int threadNumber = 0;
-    private String contentId = null;
+	private int threadNumber = 0;
+	private String contentId = null;
 
-    public Main () {
-    }
+	public Main() {
+	}
 
-    public Main ( int tn, String cid ) {
-        threadNumber = tn;
-        contentId = cid;
-    }
+	public Main(int tn, String cid) {
+		threadNumber = tn;
+		contentId = cid;
+	}
 
-    public void run () {
-        System.out.println("Starting child "+threadNumber+" ("+contentId+")");
-        DefaultHttpClient cli = new DefaultHttpClient();
-        for ( int i = 0; i < numberOfViews; i++ ) {
-            try {
-                HttpPost req = new HttpPost("http://"+webHost+"/"+apiVersion+"/pageview/"+contentId);
-                HttpResponse resp = cli.execute(req);
+	@Override
+	public void run() {
+		System.out.println("Starting child " + threadNumber + " (" + contentId + ")");
+		HttpClient cli = HttpClient.newBuilder().version(Version.HTTP_2).build();
 
-                if ( resp.getStatusLine().getStatusCode() != HttpStatus.SC_NO_CONTENT ) {
-                    String body = IOUtils.toString(resp.getEntity().getContent(), "UTF-8");
-                    System.err.println("Could not post pageview: "+body);
-                    System.exit(1);
-                }
+		for (int i = 0; i < numberOfViews; i++) {
+			try {
+				HttpRequest req = HttpRequest.newBuilder()
+						.uri(URI.create("http://" + webHost + "/" + apiVersion + "/pageview/" + contentId))
+						.POST(BodyPublishers.noBody()).build();
 
-                msgCounter.incrementAndGet();
-            } catch (IOException e) {
-                System.err.println("Could not make post request: "+e.getMessage());
-                System.exit(1);
-            }
-        }
-    }
+				HttpResponse<String> resp = cli.send(req, BodyHandlers.ofString());
 
-    public static ArrayList<String> getContentIds () throws Exception {
-        DefaultHttpClient cli = new DefaultHttpClient();
-        HttpGet req = new HttpGet("http://"+webHost+"/"+urlBase);
+				if (resp.statusCode() != 204) {
+					System.err.println("Could not post pageview: " + resp.body());
+					System.exit(1);
+				}
 
-        HttpResponse resp = cli.execute(req);
-        String body = IOUtils.toString(resp.getEntity().getContent(), "UTF-8");
+				msgCounter.incrementAndGet();
+			} catch (IOException | InterruptedException e) {
+				System.err.println("Could not make post request: " + e.getMessage());
+				System.exit(1);
+			}
+		}
+	}
 
-        if ( resp.getStatusLine().getStatusCode() != HttpStatus.SC_OK )
-            throw new Exception("Could not get content list: "+body);
+	public static ArrayList<String> getContentIds() throws Exception {
+		HttpClient cli = HttpClient.newBuilder().version(Version.HTTP_2).build();
 
-        ArrayList<String> contentIds = new ArrayList<String>();
-        JsonObject payload = (new JsonParser().parse(body)).getAsJsonObject();
-        if ( payload.has("data") && payload.get("data").isJsonArray()) {
-            for ( JsonElement cid : payload.getAsJsonArray("data") )
-                contentIds.add(cid.getAsJsonObject().get("id").getAsString());
-        }
+		HttpRequest req = HttpRequest.newBuilder().uri(URI.create("http://" + webHost + "/" + urlBase)).GET().build();
 
-        return contentIds;
-    }
+		HttpResponse<String> resp = cli.send(req, BodyHandlers.ofString());
+		String body = resp.body();
 
-    public static void main ( String[] args ) {
-        ArrayList<String> contentIds = null;
-        try {
-            contentIds = getContentIds();
-        } catch (Exception e) {
-            System.err.println("Could not get content id list: "+e.getMessage());
-            System.exit(1);
-        }
+		if (resp.statusCode() != 200)
+			throw new Exception("Could not get content list: " + body);
 
-        if ( contentIds != null ) {
+		ArrayList<String> contentIds = new ArrayList<String>();
+		JsonObject payload = (new JsonParser().parse(body)).getAsJsonObject();
+		if (payload.has("data") && payload.get("data").isJsonArray()) {
+			for (JsonElement cid : payload.getAsJsonArray("data"))
+				contentIds.add(cid.getAsJsonObject().get("id").getAsString());
+		}
 
-            ArrayList<Main> children = new ArrayList<Main>();
+		return contentIds;
+	}
 
-            Thread thread = new Thread() {
-                public void run() {
-                    while ( true ) {
-                        try {
-                            Thread.sleep(statsTimeSec * 1000);
-                        } catch (InterruptedException e) {
-                            break;
-                        }
-                        int msgCount = msgCounter.getAndSet(0);
-                        System.out.println(((float)msgCount / (float)statsTimeSec)+" msgs/sec");
-                    }
-                }
-            };
-            thread.start();
+	public static void main(String[] args) {
+		ArrayList<String> contentIds = null;
+		try {
+			contentIds = getContentIds();
+		} catch (Exception e) {
+			System.err.println("Could not get content id list: " + e.getMessage());
+			System.exit(1);
+		}
 
-            int cidIdx = 0;
-            for ( int i = 0; i < numberOfClients; i ++ ) {
-                if ( cidIdx >= contentIds.size() )
-                    cidIdx = 0;
+		if (contentIds != null) {
 
-                Main c = new Main(i+1, contentIds.get(cidIdx++));
-                c.start();
-                children.add(c);
-            }
+			ArrayList<Main> children = new ArrayList<Main>();
 
-            for ( Main c : children ) {
-                try {
-                    c.join();
-                } catch ( InterruptedException e ) {
-                    System.err.println("Error from child: "+e.getMessage());
-                }
-            }
+			Thread thread = new Thread() {
+				@Override
+				public void run() {
+					while (true) {
+						try {
+							Thread.sleep(statsTimeSec * 1000);
+						} catch (InterruptedException e) {
+							break;
+						}
+						int msgCount = msgCounter.getAndSet(0);
+						System.out.println(((float) msgCount / (float) statsTimeSec) + " msgs/sec");
+					}
+				}
+			};
+			thread.start();
 
-            thread.interrupt();
-            try {
-                thread.join();
-            } catch (InterruptedException e) {}
+			int cidIdx = 0;
+			for (int i = 0; i < numberOfClients; i++) {
+				if (cidIdx >= contentIds.size())
+					cidIdx = 0;
 
-        }
+				Main c = new Main(i + 1, contentIds.get(cidIdx++));
+				c.start();
+				children.add(c);
+			}
 
-        System.exit(0);
-    }
+			for (Main c : children) {
+				try {
+					c.join();
+				} catch (InterruptedException e) {
+					System.err.println("Error from child: " + e.getMessage());
+				}
+			}
+
+			thread.interrupt();
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+			}
+
+		}
+
+		System.exit(0);
+	}
 }
