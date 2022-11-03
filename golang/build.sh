8deab6723e0aa47f510e7a7e9961de33d8746eaf
Tim Burrington <tgburrin>
2022-11-03 08:27:41 -0400

    Further fixes for building the golang version of the service.

--- golang/build.sh
+++ golang/build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
-. /etc/profile.d/go.sh
+test -f /etc/profile.d/go.sh && . /etc/profile.d/go.sh
 
 BASE=`realpath $0`
+BASE=`dirname ${BASE}`
 
 #export GOPATH=`dirname $BASE`/pageviewcount_service
-echo "Using a base path of $GOPATH"
+echo "Using a base path of ${BASE} with go packages in $GOPATH"
 
 cd $GOPATH || exit 1
 for p in github.com/Shopify/sarama github.com/gorilla/handlers github.com/gorilla/mux github.com/jessevdk/go-flags github.com/satori/go.uuid github.com/lib/pq
@@ -13,7 +14,7 @@ do
     go get ${p}
 done
 
-cd $GOPATH
+cd ${BASE}/pageviewcount_service
 go fmt
 go vet
 go build
